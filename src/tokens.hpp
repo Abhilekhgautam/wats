f849c141dbdc47affb995eb5d5a9699df441a6d9
abhilekh <abhilekhgautam119@gmail.com>
2024-11-25 15:29:36 +0545

    feat: Add keywords: for, while, let, from, to, in, match, if, else. Add new tokens: =>. Remove '#' as token. Define helper for Token class

--- src/tokens.hpp
+++ src/tokens.hpp
@@ -5,11 +5,11 @@
 #include <iostream>
 
 enum class TokenType{
-  PLUS = '+',
-  MINUS = '-',
-  MUL = '*',
-  DIV = '/',
-  MOD = '%',
+  PLUS ,
+  MINUS,
+  MUL,
+  DIV,
+  MOD,
   EQ,
   NOT,
   NEQ,
@@ -19,6 +19,7 @@ enum class TokenType{
   GTE,
   ID,
   NUM,
+  NEW_LINE,
   OPEN_PARENTHESIS,
   CLOSE_PARENTHESIS,
   OPEN_SQUARE,
@@ -26,10 +27,9 @@ enum class TokenType{
   OPEN_CURLY,
   CLOSE_CURLY,
   COMMA,
-  HASH,
+  ARROW,
   ASSIGN,
   NUMBER,
-
   // Keywords
   EXPORT,
   IMPORT,
@@ -38,11 +38,21 @@ enum class TokenType{
   F32,
   F64,
   LOOP,
+  FOR,
+  WHILE,
   FUNCTION,
+  LET,
   CONST,
   WHERE,
-  FROM
+  FROM,
+  TO,
+  IN,
+  MATCH,
+  IF,
+  ELSE
 };
+
+std::ostream& operator<<(std::ostream& os, TokenType type);
 
 class Token{
   public:
@@ -51,9 +61,14 @@ class Token{
     Token(TokenType tok_type, std::size_t line, std::size_t pos, char tok_value): tok_type(tok_type), line(line), pos(pos), tok_value(std::string{tok_value}){}
     
     friend std::ostream& operator << (std::ostream& os, Token tok){
-	os << "line: " << tok.line << "col: " << tok.pos << " "<< tok.tok_value;    
+	os << "line: " << tok.line << " col: " << tok.pos << " "<< tok.tok_value << " " << tok.tok_type;    
 	return os;
     }
+    inline std::string GetValue(){return tok_value;}
+    inline std::size_t GetLine(){return line;}
+    inline std::size_t GetColumn(){return pos;}
+    inline TokenType GetTokenType(){return tok_type;}
+    inline bool IsIdentifier(){ return tok_type == TokenType::ID;}
   private:
     TokenType tok_type;
     std::size_t line;

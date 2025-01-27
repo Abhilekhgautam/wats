a6a431a9bdfa05e159d80302733ba6f918cef3d3
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-01-27 11:24:14 +0000

    define nodes for AST of various statements and expressions namely: identifier, number, matcharm, range expr, match statement, if, else, elseif

--- src/AST/ASTNode.hpp
+++ src/AST/ASTNode.hpp
@@ -1,4 +1,6 @@
-#include <ASTType.hpp>
+#include <vector>
+#include "ASTType.hpp"
+
 /*
 class ASTNode{
   public:
@@ -13,9 +15,9 @@ class StatementAST {
 
 class ExpressionAST {
   public:
-   Type GetNodeType() { return type;} 
+   Type* GetNodeType() { return type;}
   private:
-    Type type;
+    Type* type;
 };
 
 /// function function_name ( para1, para2 ) {
@@ -23,23 +25,26 @@ class ExpressionAST {
 /// }
 class FunctionDefinitionAST : public StatementAST{
   public:
-    FunctionDefinition(std::string_view fn_name, std::vector<std::pair<Type, std::string>> parameters, StatementAST* function_body, Type ret_type)
+    FunctionDefinitionAST(std::string_view fn_name, std::vector<std::pair<Type*, std::string>> parameters, StatementAST* function_body, Type* ret_type)
 	    : fn_name(fn_name), parameters(parameters), function_body(function_body), ret_type(ret_type){}
   private:
     std::string fn_name;
-    std::vector<std::pair<Type, std::string> parameter;
+    std::vector<std::pair<Type*, std::string>> parameters;
     StatementAST* function_body;
-    Type ret_type;
+    Type* ret_type;
+
+  public:
+    std::string GetFunctionName(){return fn_name;}
 };
 
 /// 1 to 200
-class RangeStatementAST : public StatementAST{
- public:
-   RangeStatementAST(ExpressionAST* expr_first, ExpressionAST* expr_second):
-	   expr_first(expr_first), expr_seconde(expr_second){}
- private:
-   ExpressionAST* expr_first;
-   ExpressionAST* expr_second;
+class RangeAST : public ExpressionAST {
+    public:
+       RangeAST(ExpressionAST* start, ExpressionAST* end)
+           : start(start), end(end){}
+    private:
+       ExpressionAST* start;
+       ExpressionAST* end;
 };
 
 /// for i in 1 to 200 {
@@ -47,11 +52,11 @@ class RangeStatementAST : public StatementAST{
 /// }
 class ForLoopAST : public StatementAST{
   public:
-   ForLoop(std::string iter_var_name, RangeStatementAST* range, StatementAST* loop_body) 
+   ForLoopAST(std::string iter_var_name, RangeAST* range, StatementAST* loop_body)
 	   : var_name(iter_var_name), range(range), loop_body(loop_body){}
   private:
    std::string var_name;
-   RangeStatementAST* range;
+   RangeAST* range;
    StatementAST* loop_body;
 };
 
@@ -60,7 +65,7 @@ class ForLoopAST : public StatementAST{
 /// }
 class WhileLoopAST : public StatementAST{
    public:
-    WhileLoopAST(const ExpressionAST* condition, StatementAST* loop_body)
+    WhileLoopAST(ExpressionAST* condition, StatementAST* loop_body)
 	    : condition(condition), loop_body(loop_body){}
    private:
     ExpressionAST* condition;
@@ -72,8 +77,10 @@ class WhileLoopAST : public StatementAST{
 /// }
 class LoopAST : public StatementAST{
    public:
-    LoopAST(StatementAST* loop_body) 
+    LoopAST(StatementAST* loop_body)
 	    : loop_body(loop_body){}
+	private:
+	 StatementAST* loop_body;
 
 };
 
@@ -81,11 +88,14 @@ class LoopAST : public StatementAST{
 /// let x: i32
 class VariableDeclarationAST : public StatementAST{
    public:
-     VariableDeclarationAST(Type type_name, const std::string variable_name)
+     VariableDeclarationAST(Type* type_name, const std::string variable_name)
 	     : type_name(type_name), variable_name(variable_name){}
    private:
-     Type type_name;
+     Type* type_name;
      std::string variable_name;
+
+    public:
+     std::string GetVarName(){return variable_name;}
 };
 
 /// x = 56
@@ -95,21 +105,66 @@ class VariableAssignmentAST : public StatementAST{
 	    variable_name(variable_name), expr(expr){}
   private:
     std::string variable_name;
-    ExpressionAST* AST;
-};
-
-/// let x = 45
-class VariableDeclareAndAssignAST : public Statement{
-  public:
-    VariableDeclareAndAssignAST(const std::string variable_name, Type type_name, ExpressionAST* expr)
-	    : variable_name(variable_name), type_name(type_name), expr(expr){}
-  private:
-    std::string variable_name;
-    Type type_name;
     ExpressionAST* expr;
 };
 
-class BinaryExpressionAST : public Expression {
+/// let x = 45
+class VariableDeclareAndAssignAST : public StatementAST{
+  public:
+    VariableDeclareAndAssignAST(std::string variable_name, Type* type_name, ExpressionAST* expr)
+	    : variable_name(variable_name), type_name(type_name), expr(expr){}
+  private:
+    std::string variable_name;
+    Type* type_name;
+    ExpressionAST* expr;
+};
+
+class IfStatementAST : public StatementAST{
+ public:
+    IfStatementAST(ExpressionAST* condition, StatementAST* if_body)
+       : condition(condition), if_body(if_body){}
+ private:
+    ExpressionAST* condition;
+    StatementAST* if_body;
+};
+
+class ElseStatementAST : public StatementAST{
+ public:
+   ElseStatementAST(StatementAST* else_body)
+	: else_body(else_body){}
+ private:
+   StatementAST* else_body;
+};
+
+class ElseIfStatementAST : public StatementAST{
+  public:
+    ElseIfStatementAST(ExpressionAST* condition, StatementAST* else_if_body)
+	: condition(condition), else_if_body(else_if_body){}
+  private:
+    ExpressionAST* condition;
+    StatementAST* else_if_body;
+};
+
+class MatchArmAST : public StatementAST{
+    public:
+       MatchArmAST(ExpressionAST* cond, StatementAST* body)
+       : cond(cond), body(body){}
+    private:
+      ExpressionAST* cond;
+      StatementAST* body;
+};
+
+class MatchStatementAST : public StatementAST{
+  public:
+     MatchStatementAST(ExpressionAST* cond, std::vector<MatchArmAST*>& arms)
+     : cond(cond), arms(arms){}
+  private:
+    ExpressionAST* cond;
+    std::vector<MatchArmAST*> arms;
+};
+
+
+class BinaryExpressionAST : public ExpressionAST{
   public:
     BinaryExpressionAST(ExpressionAST* expr_lhs, ExpressionAST* expr_rhs, std::string operator_symbol)
 	    : expr_lhs(expr_lhs), expr_rhs(expr_rhs), operator_symbol(operator_symbol){}
@@ -118,4 +173,19 @@ class BinaryExpressionAST : public Expression {
     ExpressionAST* expr_rhs;
     std::string operator_symbol;
 
+};
+
+class IdentifierAST : public ExpressionAST {
+    public:
+      IdentifierAST(std::string name):
+      name(name){}
+    private:
+    std::string name;
+};
+
+class NumberAST : public ExpressionAST {
+    public:
+       NumberAST(std::string num): num(num){}
+    private:
+      std::string num;
 };

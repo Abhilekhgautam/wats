5be103ee94d2b6a928d55686785f19bc20aed9d1
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-02-01 06:57:33 +0000

    include emscripten header and function to be exported while wasm build

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 #include "cmd-parser.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 #include "utils.hpp"
 
+#ifdef __EMSCRIPTEN__
+extern "C" {
+    EMSCRIPTEN_KEEPALIVE
+    void compile_program(const char* source_code) {
+        std::string test(source_code);
+
+        Lexer L(test);
+        L.Tokenize();
+        L.Debug();
+
+        Parser P(L.GetTokens(), L.GetSourceCode());
+        P.Parse();
+    }
+}
+#endif
+
 int main(int argc, char **argv) {
 
   if (!verify_args(argc, argv)){
-     return 0;	  
+     return 0;
   }
 
   std::string test = read_file(std::filesystem::path{argv[1]});

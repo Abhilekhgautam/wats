dde3a3b3c21ae71f1a31f59076803232a202c91e
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-02-09 13:17:52 +0000

    fix: only print fn args when there is one

--- src/AST/FunctionDefinitionAST.cpp
+++ src/AST/FunctionDefinitionAST.cpp
@@ -4,8 +4,10 @@
 void FunctionDefinitionAST::Debug(){
     std::cout << "Function: " << fn_name << '\n';
     std::cout << "Function Arguments:\n";
-    for(const auto& elt: arguments->GetArgs()){
-        std::cout << elt << " ";
+    if (arguments){
+       for(const auto& elt: arguments->GetArgs()){
+            std::cout << elt << " ";
+        }
     }
     std::cout << '\n';
     std::cout << "Function Body:\n";

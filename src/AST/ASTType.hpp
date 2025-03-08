56e41bcb3ebe346732f8447293f1507513947fa5
AbhilekhGautam <abhilekhgautam119@gmail.com>
2025-03-08 17:28:37 +0000

    feat: Define a TypeFactory to create Types

--- src/AST/ASTType.hpp
+++ src/AST/ASTType.hpp
@@ -1,6 +1,7 @@
 #ifndef AST_TYPE
 #define AST_TYPE
 
+#include <memory>
 #include <string>
 
 class Type{
@@ -12,7 +13,7 @@ class Type{
 class PrimitiveType : public Type{
   public:
     PrimitiveType(const std::string type_name): type_name(type_name){}
-    std::string GetType() const { return type_name; }
+    std::string GetType() override{ return type_name; }
 
     virtual ~PrimitiveType() = default;
 
@@ -23,12 +24,30 @@ class PrimitiveType : public Type{
 class UserDefinedType : public Type{
   public:
     UserDefinedType(const std::string type_name) : type_name(type_name){}
-    std::string GetType() const{ return type_name;}
+    std::string GetType() override{ return type_name;}
 
     virtual ~UserDefinedType() = default;
 
   private:
     std::string type_name;
+};
+
+class TypeFactory{
+    public:
+     static std::unique_ptr<Type> CreateType(std::string name){
+         if (name == "i32"){
+             return std::make_unique<PrimitiveType>(name);
+         } else if (name == "i64"){
+             return std::make_unique<PrimitiveType>(name);
+         } else if (name == "f32"){
+             return std::make_unique<PrimitiveType>(name);
+         } else if (name == "f64"){
+             return std::make_unique<PrimitiveType>(name);
+         }
+         else {
+             return std::make_unique<UserDefinedType>(name);
+         }
+     }
 };
 
 #endif

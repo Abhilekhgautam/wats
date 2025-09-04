e9e1e034a37c883fe77f5d3b33a442fbbea789ca
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-09-05 00:06:59 +0545

    feat: check for errors in every stage and only proceed if the preceeding stage has no errors

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,11 @@
 #include "CompilerContext.hpp"
 #include "cmd-parser.hpp"
 
+#include "IRGenerator/IRGenerator.hpp"
 #include "utils.hpp"
+#include <nlohmann/json.hpp>
 
+using nlohmann::json;
 #ifdef __EMSCRIPTEN__
 extern "C" {
 EMSCRIPTEN_KEEPALIVE
@@ -27,13 +30,30 @@ void compile_program(const char *source_code) {
   SemanticAnalyzer s(context, ast_vec.value());
   s.analyze();
 
+  nlohmann::json program = {{"functions", nlohmann::json::array({})}};
+
   // Only for debug
   if (ast_vec.has_value()) {
     auto stmt_vec = std::move(ast_vec.value());
     for (const auto &elt : stmt_vec) {
-      elt->Debug();
+      if (auto val = dynamic_cast<FunctionDefinitionAST *>(elt.get())) {
+        std::vector<nlohmann::json> instrVec;
+        std::string fnName;
+
+        fnName = val->GetFunctionName();
+        // IRContext ctx(fnName);
+        IRGenerator brilGenerator;
+        for (const auto &elt : val->GetFunctionBody()) {
+          instrVec.push_back(brilGenerator.Generate(*elt));
+        }
+
+        nlohmann::json fn_obj = {{"name", fnName}, {"instrs", instrVec}};
+
+        program["functions"].push_back(fn_obj);
+      }
     }
   }
+  std::cout << program.dump(4) << '\n';
 }
 }
 #endif
@@ -50,19 +70,69 @@ int main(int argc, char **argv) {
 
   Lexer L(context);
   L.Tokenize();
-  L.Debug();
 
   Parser P(context, L.GetTokens());
+  // function name, var name ....
   auto ast_vec = P.Parse();
+
+  if (P.HasErrors()) {
+    return 1;
+  }
 
   SemanticAnalyzer s(context, ast_vec.value());
   s.analyze();
+
+  if (s.HasErrors()) {
+    return 0;
+  }
+
+  nlohmann::json program = {{"functions", nlohmann::json::array({})}};
 
   // Only for debug
   if (ast_vec.has_value()) {
     auto stmt_vec = std::move(ast_vec.value());
     for (const auto &elt : stmt_vec) {
-      elt->Debug();
+      if (auto val = dynamic_cast<FunctionDefinitionAST *>(elt.get())) {
+        std::vector<nlohmann::json> instrVec;
+        std::string fnName;
+
+        fnName = val->GetFunctionName();
+
+        std::map<std::string, std::string> name2type;
+        auto args = val->GetFunctionArguments();
+
+        std::vector<std::string> argVec;
+        if (args.has_value()) {
+          for (const auto &elt : args->get().GetArgs()) {
+            name2type[elt] = "i64";
+          }
+          argVec = args->get().GetArgs();
+        }
+
+        // IRContext ctx(fnName);
+        IRGenerator brilGenerator;
+        for (const auto &elt : val->GetFunctionBody()) {
+          json generated_json = brilGenerator.Generate(*elt);
+          if (!generated_json.is_array())
+            instrVec.push_back(brilGenerator.Generate(*elt));
+          else {
+            for (const auto &elt : generated_json) {
+              instrVec.push_back(elt);
+            }
+          }
+        }
+
+        json fnArgs = json::array({});
+        for (const auto &[name, type] : name2type) {
+          fnArgs.push_back({{"name", name}, {"type", type}});
+        }
+
+        nlohmann::json fn_obj = {
+            {"name", fnName}, {"instrs", instrVec}, {"args", fnArgs}};
+
+        program["functions"].push_back(fn_obj);
+      }
     }
   }
+  std::cout << program.dump(4);
 }

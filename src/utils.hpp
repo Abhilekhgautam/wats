868f9f901252d16b0da584ec891beafbf7e375fa
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-09-05 00:06:16 +0545

    feat: support for multiple spaced arrows in error reporting

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdarg>
 
 std::string read_file(std::filesystem::path path);
 std::vector<std::string> split_str(const std::string &text,
@@ -53,15 +54,28 @@ inline void Color(std::string color, std::string line, bool newLine = false) {
   }
 }
 
+
 #endif
-inline std::string SetArrowLeft(std::size_t pos, std::size_t times = 1) {
+inline std::string SetArrowLeft(int pos, int times = 1) {
   std::string arrows;
-  for (size_t i = pos - times; i > 0; --i)
+  for (int i = pos - times; i > 0; --i)
     arrows += ' ';
-  for (size_t i = 0; i < times; ++i)
+  for (int i = 0; i < times; ++i)
     arrows += '^';
   return arrows;
 }
+
+inline std::string SetArrowLeft(){
+    return "";
+}
+
+template <typename... TArgs>
+inline std::string MultiPartArrow(int start, int len, TArgs... rest) {
+    std::string current = SetArrowLeft(start, len);
+    std::string tail = SetArrowLeft(rest...).erase(start, len);
+    return current + tail;
+}
+
 inline std::string SetArrow(std::size_t pos, std::size_t times = 1) {
   std::string arrows;
   for (size_t i = 0; i < pos - 1; ++i)
@@ -80,6 +94,7 @@ inline std::string SetPlus(std::size_t pos, std::size_t times = 1) {
 
   return plus;
 }
+
 #endif
 
 #endif

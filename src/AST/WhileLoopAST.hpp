5946655b6a80fcacbd8a88b47def3c83a55e1c7e
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-09-07 14:08:24 +0545

    add helper to get the loop body.

--- src/AST/WhileLoopAST.hpp
+++ src/AST/WhileLoopAST.hpp
@@ -1,27 +1,34 @@
 #ifndef WHILE_AST
 #define WHILE_AST
 
-#include "StatementAST.hpp"
 #include "ExpressionAST.hpp"
+#include "StatementAST.hpp"
 #include <vector>
 /// while i < 5 {
 ///     # loop body
 /// }
-class WhileLoopAST : public StatementAST{
-   public:
-    WhileLoopAST(std::unique_ptr<ExpressionAST> condition, std::vector<std::unique_ptr<StatementAST>> loop_body)
-	    : condition(std::move(condition)), loop_body(std::move(loop_body)){}
+class WhileLoopAST : public StatementAST {
+public:
+  WhileLoopAST(std::unique_ptr<ExpressionAST> condition,
+               std::vector<std::unique_ptr<StatementAST>> loop_body)
+      : condition(std::move(condition)), loop_body(std::move(loop_body)) {}
 
-	virtual ~WhileLoopAST() = default;
-	void Accept(SemanticAnalyzer& analyzer) override;
-    nlohmann::json Accept(IRGenerator& generator) override;
+  virtual ~WhileLoopAST() = default;
 
-   private:
-    std::unique_ptr<ExpressionAST> condition;
-    std::vector<std::unique_ptr<StatementAST>>  loop_body;
-   public:
-   void Debug() override;
-   SourceLocation GetSourceLocation() override {return {};}
+  ExpressionAST &GetCondition() { return *condition; }
+
+  std::vector<std::unique_ptr<StatementAST>> &GetBody() { return loop_body; }
+
+  void Accept(SemanticAnalyzer &analyzer) override;
+  nlohmann::json Accept(IRGenerator &generator) override;
+
+private:
+  std::unique_ptr<ExpressionAST> condition;
+  std::vector<std::unique_ptr<StatementAST>> loop_body;
+
+public:
+  void Debug() override;
+  SourceLocation GetSourceLocation() override { return {}; }
 };
 
 #endif

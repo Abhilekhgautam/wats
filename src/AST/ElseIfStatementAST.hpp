133eedcd5c6059f28455c1746b6091113fcb11b2
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2025-09-07 13:45:29 +0545

    add helper to get body.

--- src/AST/ElseIfStatementAST.hpp
+++ src/AST/ElseIfStatementAST.hpp
@@ -1,29 +1,36 @@
 #ifndef ELSE_IF_AST
 #define ELSE_IF_AST
 
-#include "StatementAST.hpp"
 #include "ExpressionAST.hpp"
+#include "StatementAST.hpp"
 
-#include <vector>
 #include <memory>
+#include <vector>
 
-class ElseIfStatementAST : public StatementAST{
-  public:
-    ElseIfStatementAST(std::unique_ptr<ExpressionAST> condition, std::vector<std::unique_ptr<StatementAST>> else_if_body, SourceLocation loc)
-	: condition(std::move(condition)), else_if_body(std::move(else_if_body)), loc(loc){}
+class ElseIfStatementAST : public StatementAST {
+public:
+  ElseIfStatementAST(std::unique_ptr<ExpressionAST> condition,
+                     std::vector<std::unique_ptr<StatementAST>> else_if_body,
+                     SourceLocation loc)
+      : condition(std::move(condition)), else_if_body(std::move(else_if_body)),
+        loc(loc) {}
 
-	virtual ~ElseIfStatementAST() = default;
+  virtual ~ElseIfStatementAST() = default;
 
-	void Accept(SemanticAnalyzer& analyzer) override;
-	nlohmann::json Accept(IRGenerator& generator) override;
+  ExpressionAST &GetCondition() { return *condition; }
+  std::vector<std::unique_ptr<StatementAST>> &GetBody() { return else_if_body; }
 
-   private:
-    std::unique_ptr<ExpressionAST> condition;
-    std::vector<std::unique_ptr<StatementAST>> else_if_body;
-    SourceLocation loc;
-    public:
-       void Debug() override;
-       SourceLocation GetSourceLocation() override {return loc;}
+  void Accept(SemanticAnalyzer &analyzer) override;
+  nlohmann::json Accept(IRGenerator &generator) override;
+
+private:
+  std::unique_ptr<ExpressionAST> condition;
+  std::vector<std::unique_ptr<StatementAST>> else_if_body;
+  SourceLocation loc;
+
+public:
+  void Debug() override;
+  SourceLocation GetSourceLocation() override { return loc; }
 };
 
 #endif
--- src/AST/ElseStatementAST.hpp
+++ src/AST/ElseStatementAST.hpp
@@ -6,23 +6,26 @@
 #include <memory>
 #include <vector>
 
-class ElseStatementAST : public StatementAST{
- public:
-   ElseStatementAST(std::vector<std::unique_ptr<StatementAST>> else_body, SourceLocation loc)
-	: else_body(std::move(else_body)), loc(loc){}
+class ElseStatementAST : public StatementAST {
+public:
+  ElseStatementAST(std::vector<std::unique_ptr<StatementAST>> else_body,
+                   SourceLocation loc)
+      : else_body(std::move(else_body)), loc(loc) {}
 
-   virtual ~ElseStatementAST() = default;
+  virtual ~ElseStatementAST() = default;
 
-   void Accept(SemanticAnalyzer& analyzer) override;
-   nlohmann::json Accept(IRGenerator& generator) override;
+  std::vector<std::unique_ptr<StatementAST>> &GetBody() { return else_body; }
 
+  void Accept(SemanticAnalyzer &analyzer) override;
+  nlohmann::json Accept(IRGenerator &generator) override;
 
- private:
-   std::vector<std::unique_ptr<StatementAST>> else_body;
-   SourceLocation loc;
-   public:
-      void Debug() override;
-      SourceLocation GetSourceLocation() override {return loc;}
+private:
+  std::vector<std::unique_ptr<StatementAST>> else_body;
+  SourceLocation loc;
+
+public:
+  void Debug() override;
+  SourceLocation GetSourceLocation() override { return loc; }
 };
 
 #endif

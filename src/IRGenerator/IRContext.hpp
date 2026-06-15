c0cad961d5f262c0422019b2bc0aa2dcb41864bc
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-15 21:50:01 +0545

    update the IR Context to have break point and skip point names

--- src/IRGenerator/IRContext.hpp
+++ src/IRGenerator/IRContext.hpp
@@ -3,18 +3,26 @@
 
 #include <string>
 
-template <typename T> class IRContext {
+// This class holds the necessary information required to generate the IR
+// For eg: where to break to , if a break statement pops inside an if.
+class IRContext {
 public:
-  IRContext(std::string name) : fn_name(name), tempVarCount(0) {}
+  IRContext() : tempVarCount(0) {}
 
 private:
-  std::string fn_name;
+  std::string break_point_name {}; // this is where the break statement jump to.
+  std::string skip_point_name {}; // this is where the skip statement jump to.
   int tempVarCount;
 
 public:
-  std::string GetFnName() { return fn_name; }
-  void IncrementTempCount() { tempVarCount = tempVarCount + 1; }
-  int GetCurrentCount() { return tempVarCount; }
+  void IncrementTempCount() { tempVarCount = tempVarCount + 1;}
+
+  [[nodiscard]] int GetCurrentCount() { return tempVarCount; }
+  void SetBreakPointName(const std::string& bp ) {break_point_name = bp;}
+  void SetSkipPointName(const std::string& sp) {skip_point_name = sp;}
+
+  std::string GetBreakPointName() {return break_point_name;}
+  std::string GetSkipPointName() {return skip_point_name;}
 };
 
 #endif

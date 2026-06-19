95b44bbdeda9f4e574433ce1bd74edde28716887
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-19 23:48:01 +0545

    impl base class for all the passes

---
+++ src/opt/passes/Pass.h
@@ -0,0 +1,18 @@
+//
+// Created by void on 6/19/26.
+//
+
+#ifndef WATS_PASS_H
+#define WATS_PASS_H
+
+#include <nlohmann/json.hpp>
+#include <string>
+
+class Pass {
+public:
+    virtual ~Pass() = default;
+    virtual std::string name() = 0;
+    virtual void run(std::vector<nlohmann::json>&) = 0;
+};
+
+#endif // WATS_PASS_H

8963a77980b3511c199719895efad35c093053a8
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-21 01:30:04 +0545

    update the run method to return results

--- src/opt/analysis/LVN.cpp
+++ src/opt/analysis/LVN.cpp
@@ -3,3 +3,73 @@
 //
 
 #include "LVN.h"
+#include <iostream>
+
+LVNResult LVN::run(const std::vector<nlohmann::json>& instrs) {
+    std::unordered_map<std::string, int> lvn_environment;
+    LVNTable table;
+
+    int index = 1;
+    for (auto& instr : instrs) {
+       // Handle constant assignments.
+       if (instr.contains("value")) {
+          LVNTableValue value{instr["value"].get<int>(), {}, {}, "const"};
+
+           if (instr.contains("dest")) {
+               if (!table.containsValue(value)) {
+                   table.addValue(index, instr["dest"], value);
+                   lvn_environment[instr["dest"]] = index;
+                   index = index + 1;
+               }
+               else {
+                   // Safety: Guaranteed to be there, confirmed by the above if.
+                   lvn_environment[instr["dest"]] = table.getIndex(value).value();
+               }
+           }
+       }
+       else if (instr.contains("args")) {
+           auto args = instr["args"].get<std::vector<std::string>>();
+           // Check if it is an arithmetic op
+           if (args.size() == 2) {
+               int lhs = lvn_environment[args[0]];
+               int rhs = lvn_environment[args[1]];
+               if (instr["op"] == "mul" || instr["op"] == "add") {
+                  if (lhs > rhs) {
+                      std::swap(lhs, rhs);
+                  }
+               }
+               LVNTableValue value{{}, lhs, rhs, instr["op"]};
+
+               if (instr.contains("dest")) {
+                   if (!table.containsValue(value)) {
+                       table.addValue(index, instr["dest"], value);
+                       lvn_environment[instr["dest"]] = index;
+                       index = index + 1;
+                   }
+                   else {
+                       // Safety: Guaranteed to be there, confirmed by the above if.
+                       lvn_environment[instr["dest"]] = table.getIndex(value).value();
+                   }
+               }
+           }
+           else if (args.size() == 1 && instr["op"] == "id") {
+               LVNTableValue value{{}, lvn_environment[instr["args"][0]], {}, "id"};
+
+               if (instr.contains("dest")) {
+                   if (!table.containsValue(value)) {
+                       table.addValue(index, instr["dest"], value);
+                       lvn_environment[instr["dest"]] = index;
+                       index = index + 1;
+                   } else {
+                       // Safety: Guaranteed to be there, confirmed by the above if.
+                       lvn_environment[instr["dest"]] = table.getIndex(value).value();
+                   }
+
+               }
+           }
+       }
+
+    }
+
+    return {lvn_environment, table};
+}
--- src/opt/analysis/LVN.h
+++ src/opt/analysis/LVN.h
@@ -5,8 +5,92 @@
 #ifndef WATS_LVN_H
 #define WATS_LVN_H
 
+#include <string>
+#include <ostream>
+#include <nlohmann/json.hpp>
+
+
+struct LVNTableValue {
+    std::optional<int> value;
+    std::optional<int> lhs;
+    std::optional<int> rhs;
+    std::string op;
+
+    bool operator == (const LVNTableValue& other) const{
+        return (value == other.value && op == other.op && lhs == other.lhs && rhs == other.rhs);
+    }
+
+    friend std::ostream& operator << (std::ostream& os, LVNTableValue other) {
+        if (other.value.has_value()) {
+            os << other.op << " " << other.value.value();
+        }
+        else if (other.lhs.has_value() && other.rhs.has_value()) {
+            os << other.op << " " << other.lhs.value() << " " << other.rhs.value();
+        }
+
+        else if (other.lhs.has_value()) {
+            os << other.op << " " << other.lhs.value();
+
+        }
+
+        return os;
+    }
+};
+
+struct LVNTable {
+    std::unordered_map<int, LVNTableValue> index2Value;
+    std::unordered_map<int, std::string> index2Name;
+
+    void addValue(const int index, const std::string& name, const LVNTableValue& value) {
+        index2Name[index] = name;
+        index2Value[index] = value;
+    }
+    bool containsValue(const LVNTableValue& value) {
+        for (const auto& [id, v] : index2Value) {
+            if (v == value) return true;
+        }
+        return false;
+    }
+
+    bool containsName(const std::string& name) {
+        for (const auto& [id, v] : index2Name) {
+            if (v == name) return true;
+        }
+        return false;
+    }
+
+    bool containsValue(const LVNTableValue& value) const {
+        for (const auto& [id, v] : index2Value) {
+            if (v == value) return true;
+        }
+        return false;
+    }
+
+    bool containsName(const std::string& name) const {
+        for (const auto& [id, v] : index2Name) {
+            if (v == name) return true;
+        }
+        return false;
+    }
+
+    std::optional<int> getIndex(const LVNTableValue& value) {
+        for (const auto& [id, v] : index2Value) {
+            if (v == value) return id;
+        }
+        // Caller must ensure the value was previously added
+        // Unreachable, just to supress warning
+        return {};
+    }
+};
+
+struct LVNResult {
+    std::unordered_map<std::string, int> lvn_environment;
+    LVNTable table;
+};
 
 class LVN {
+public:
+    static LVNResult run(const std::vector<nlohmann::json>&);
 };
 
 

4aa2bd6d02401340c894f0af9a5a16e867138665
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-21 01:28:14 +0545

    feat: add cse pass

---
+++ src/opt/passes/CSE.cpp
@@ -0,0 +1,50 @@
+//
+// Created by void on 6/20/26.
+//
+
+#include "CSE.h"
+
+#include "../CFGBuilder.h"
+#include "../analysis/LVN.h"
+
+void CSE::run(std::vector<nlohmann::json>& instrs) {
+    auto CFG = CFGBuilder::build(instrs);
+    for (auto& block: CFG) {
+        const auto& [lvn_environment, lvn_table] = LVN::run(block.instrs);
+
+        std::vector<nlohmann::json> optimized_instrs;
+        for (auto& instr : block.instrs) {
+            if (instr.contains("dest")) {
+                if (lvn_environment.contains(instr["dest"]) && !lvn_table.containsName(instr["dest"])) {
+                    const int index = lvn_environment.at(instr["dest"]);
+                    const std::string canonical_var = lvn_table.index2Name.at(index);
+
+                    const nlohmann::json new_instr = {
+                        {"op", "id"},
+                        {"args", {canonical_var}},
+                            {"dest", instr["dest"]}
+                    };
+                    optimized_instrs.push_back(new_instr);
+                }
+                else {
+                    optimized_instrs.push_back(instr);
+                }
+            }
+            else {
+                optimized_instrs.push_back(instr);
+            }
+        }
+
+        block.instrs = optimized_instrs;
+    }
+    std::vector<nlohmann::json> output;
+
+    for (auto& block : CFG) {
+        output.push_back({{
+      "label", block.block_name}}
+      );
+        output.insert(output.end(), block.instrs.begin(), block.instrs.end());
+    }
+
+    instrs = output;
+}
---
+++ src/opt/passes/CSE.h
@@ -0,0 +1,17 @@
+//
+// Created by void on 6/20/26.
+//
+
+#ifndef WATS_CSE_H
+#define WATS_CSE_H
+
+#include "Pass.h"
+
+class CSE : public Pass{
+public:
+    std::string name() override {return "cse";}
+    void run(std::vector<nlohmann::json>&) override;
+};
+
+
+#endif //WATS_CSE_H

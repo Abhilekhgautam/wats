e8038b7d7cb7c9bd0bb6a19de06beb31a2013029
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-22 01:27:37 +0545

    use variant for lhs and rhs for lvntablevalue type

--- src/opt/analysis/LVN.h
+++ src/opt/analysis/LVN.h
@@ -7,33 +7,49 @@
 
 #include <string>
 #include <ostream>
+#include <variant>
 #include <nlohmann/json.hpp>
 
 
 struct LVNTableValue {
+    // Stores value for const and index for id.
     std::optional<int> value;
-    std::optional<int> lhs;
-    std::optional<int> rhs;
+    // lhs operand name or index
+    std::optional<std::variant<int, std::string>> lhs;
+    // rhs operand name or index
+    std::optional<std::variant<int, std::string>> rhs;
+    // Stores the operations - add , id , const
     std::string op;
 
     bool operator == (const LVNTableValue& other) const{
         return (value == other.value && op == other.op && lhs == other.lhs && rhs == other.rhs);
     }
 
-    friend std::ostream& operator << (std::ostream& os, LVNTableValue other) {
-        if (other.value.has_value()) {
-            os << other.op << " " << other.value.value();
-        }
-        else if (other.lhs.has_value() && other.rhs.has_value()) {
-            os << other.op << " " << other.lhs.value() << " " << other.rhs.value();
-        }
+    // friend std::ostream& operator << (std::ostream& os, LVNTableValue other) {
+    //     if (other.value.has_value()) {
+    //         os << other.op << " " << other.value.value();
+    //     }
+    //     else if (other.lhs.has_value() && other.rhs.has_value()) {
+    //         os << other.op << " " << other.lhs.value() << " " << other.rhs.value();
+    //     }
+    //
+    //     else if (other.lhs.has_value()) {
+    //         os << other.op << " " << other.lhs.value();
+    //
+    //     }
+    //
+    //     return os;
+    // }
 
-        else if (other.lhs.has_value()) {
-            os << other.op << " " << other.lhs.value();
+    // Does the value field contains an index
+    [[nodiscard]]bool hasIndex() const {
+        return (op == "id" && value.has_value());
+    }
 
-        }
-
-        return os;
+    // Get the index.
+    // Only call if hasIndex() returns true.
+    [[nodiscard]]int getCorrespondingIndex() const{
+        return value.value();
     }
 };
 
@@ -46,31 +62,30 @@ struct LVNTable {
         index2Value[index] = value;
     }
     bool containsValue(const LVNTableValue& value) {
-        for (const auto& [id, v] : index2Value) {
-            if (v == value) return true;
-        }
-        return false;
+        return std::ranges::any_of(index2Value | std::views::values, [value](const auto& v) {
+            return v == value;
+        });
+
     }
 
     bool containsName(const std::string& name) {
-        for (const auto& [id, v] : index2Name) {
-            if (v == name) return true;
-        }
-        return false;
+        return std::ranges::any_of(index2Name | std::views::values, [name](const auto& n) {
+            return n == name;
+        });
+
     }
 
     bool containsValue(const LVNTableValue& value) const {
-        for (const auto& [id, v] : index2Value) {
-            if (v == value) return true;
-        }
-        return false;
+        return std::ranges::any_of(index2Value | std::views::values, [value](const auto& v) {
+            return v == value;
+        });
     }
 
     bool containsName(const std::string& name) const {
-        for (const auto& [id, v] : index2Name) {
-            if (v == name) return true;
-        }
-        return false;
+        return std::ranges::any_of(index2Name | std::views::values, [name](const auto& n) {
+            return n == name;
+        });
+
     }
 
     std::optional<int> getIndex(const LVNTableValue& value) {

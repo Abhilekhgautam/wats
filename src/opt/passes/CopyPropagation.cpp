de1d3afa6f6010cc9207a35c5831cf399f694b40
Abhilekh Gautam <abhilekhgautam119@gmail.com>
2026-06-22 08:08:31 +0545

    fix: only propagate if the new index is less than current index

--- src/opt/passes/CopyPropagation.cpp
+++ src/opt/passes/CopyPropagation.cpp
@@ -11,6 +11,8 @@
 
 #include "../analysis/LVN.h"
 
+#include <iostream>
+
 void CopyPropagation::run(std::vector<nlohmann::json>& instrs) {
     auto CFG = CFGBuilder::build(instrs);
 
@@ -22,7 +24,7 @@ void CopyPropagation::run(std::vector<nlohmann::json>& instrs) {
             if (instr.contains("args")) {
                 if (instr.contains("op")) {
                     const std::string op = instr["op"];
-                    if (op == "id" || op == "ret") {
+                    if (op == "id" || op == "ret" || op == "br") {
                         auto arg = instr["args"][0];
                         if (lvn_environment.contains(arg)) {
                             const int index = lvn_environment.at(arg);
@@ -39,7 +41,16 @@ void CopyPropagation::run(std::vector<nlohmann::json>& instrs) {
                                         {"dest", instr["dest"]}
                                     };
                                     optimized_instrs.push_back(new_instr);
-                                } else {
+                                }
+                                else if (op == "br") {
+                                    const nlohmann::json new_instr = {
+                                        {"op", "br"},
+                                        {"args", {ref_var_name}},
+                                        {"labels", instr["labels"]}
+                                    };
+                                    optimized_instrs.push_back(new_instr);
+                                }
+                                else {
                                     const nlohmann::json new_instr = {
                                         {"op", "ret"},
                                         {"args", {ref_var_name}}
@@ -57,32 +68,56 @@ void CopyPropagation::run(std::vector<nlohmann::json>& instrs) {
                     // Has args, has op but is not id must be some arithmetic operations
                     else {
                         const auto& args = instr["args"].get<std::vector<std::string>>();
+                        const std::string dest = instr["dest"];
 
                         auto lhs = args[0];
                         auto rhs = args[1];
 
+                        auto dest_index = lvn_environment.at(dest);
+
                         if (lvn_environment.contains(lhs)) {
                             auto lhs_index = lvn_environment.at(lhs);
 
-                            if (auto lhs_value = lvn_table.index2Value.at(lhs_index); lhs_value.hasIndex()) {
-                                auto new_lhs_index = lhs_value.getCorrespondingIndex();
-                                lhs = lvn_table.index2Name.at(new_lhs_index);
-                            }
-                            else if (lhs_value.op == "id") {
-                                lhs = std::get<std::string>(lhs_value.lhs.value());
+                            if (lhs_index < dest_index) {
+                                if (auto lhs_value = lvn_table.index2Value.at(lhs_index); lhs_value.hasIndex()) {
+                                    auto new_lhs_index = lhs_value.getCorrespondingIndex();
+                                    if (new_lhs_index < lhs_index) {
+                                        lhs = lvn_table.index2Name.at(new_lhs_index);
+                                    }
+                                }
+                                else if (lhs_value.op == "id") {
+                                    auto possible_lhs = std::get<std::string>(lhs_value.lhs.value());
+                                    if (lvn_environment.contains(possible_lhs)) {
+                                        auto possible_rhs_index = lvn_environment.at(possible_lhs);
+                                        if (possible_rhs_index < lhs_index) {
+                                            lhs = possible_lhs;
+                                        }
+                                    }
+                                }
                             }
                         }
 
                         if (lvn_environment.contains(rhs)) {
                             auto rhs_index = lvn_environment.at(rhs);
-                            auto rhs_value = lvn_table.index2Value.at(rhs_index);
 
-                            if (rhs_value.hasIndex()) {
-                                auto new_rhs_index = rhs_value.getCorrespondingIndex();
-                                rhs = lvn_table.index2Name.at(new_rhs_index);
-                            }
-                            else if (rhs_value.op == "id") {
-                                rhs = std::get<std::string>(rhs_value.lhs.value());
+                            if (rhs_index < dest_index) {
+                                auto rhs_value = lvn_table.index2Value.at(rhs_index);
+
+                                if (rhs_value.hasIndex()) {
+                                    auto new_rhs_index = rhs_value.getCorrespondingIndex();
+                                    if (new_rhs_index < rhs_index) {
+                                        rhs = lvn_table.index2Name.at(new_rhs_index);
+                                    }
+                                }
+                                else if (rhs_value.op == "id") {
+                                    auto possible_rhs = std::get<std::string>(rhs_value.lhs.value());
+                                    if (lvn_environment.contains(possible_rhs)) {
+                                        auto possible_rhs_index = lvn_environment.at(possible_rhs);
+                                        if (possible_rhs_index < rhs_index) {
+                                            rhs = possible_rhs;
+                                        }
+                                    }
+                                }
                             }
                         }
 
